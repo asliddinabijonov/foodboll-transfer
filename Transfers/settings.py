2209a39468c477ec8973b74db5d40e6334ad891f
unknown <asliddinabijonov04@gmail.com>
2024-04-18 08:53:39 +0500

    dsfhdkjsf

--- Transfers/settings.py
+++ Transfers/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-t&dvc8@y2)^l3k^#km$-e#h95u$ia@z=$*e%7vgc+!g6&9nu#)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -109,8 +109,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
-
+# STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'static'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
